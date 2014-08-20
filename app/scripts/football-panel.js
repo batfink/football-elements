dbf253708f5ea6a776fa2a38feea7490ad44d957
Victor Nystad <vnystad@gmail.com>
2014-08-20 14:11:17 +0200

    testing xhr

--- app/scripts/football-panel.js
+++ app/scripts/football-panel.js
@@ -6,6 +6,9 @@
         createdCallback: {
             value: function () {
                 console.log('created');
+                this.readAttributes();
+
+                // console.log(this);
 
                 var frag = document.createDocumentFragment();
 
@@ -17,11 +20,54 @@
                 frag.appendChild(div);
                 this.appendChild(frag);
 
+                this.getJSON();
             }
+        },
+        readAttributes: {
+          value: function () {
+            this.operation = this.getAttribute('operation');
+            if (!!this.getAttribute('route')) {
+              this.route = this.getAttribute('route');
+            }
+            this.key = this.getAttribute('key');
+          }
+        },
+        attributeChangedCallback : {
+          value: function () {
+            console.log('attribute changed');
+          }
+        },
+        getJSON : {
+          value: function () {
+
+            var that = this, xhr = new XMLHttpRequest();
+
+            xhr.open('GET', 'http://api.fotballdata.no/v1/' + this.operation + '/' + this.key + (!!this.route ? '/' + this.route : '') + '.json', true);
+
+            xhr.onerror = function() {
+                console.log('transfer failed');
+            };
+
+            xhr.onload = function() {
+              that.buildHTML(JSON.parse(this.responseText));
+            };
+
+            xhr.ontimeout = function(){
+              console.log('timeout');
+            };
+
+            xhr.send();
+
+          }
+        },
+        buildHTML: {
+          value: function (data) {
+            console.log(data);
+          }
         }
     });
 
-    document.registerElement( 'football-panel', {
+    window.FootballPanel = document.registerElement( 'football-panel', {
         prototype: proto
     });
 
