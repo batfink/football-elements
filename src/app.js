6d2a2ccb09fb90b7bea607ff5d48cda40a96bd0d
Victor Nystad <vnystad@gmail.com>
2014-08-21 13:53:42 +0200

    added promise-module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,3 @@
 require('document-register-element');
-require('es6-promise');
+// require('es6-promise').Promise;
 require('./football-panel');
--- src/create-url.js
+++ src/create-url.js
@@ -1,3 +1,5 @@
+var Promise = require('es6-promise').Promise;
+
 function createUrl(attributes) {
   return Promise.resolve('http://api.fotballdata.no/v1/' + attributes.operation + '/' + attributes.key + (!!attributes.route ? '/' + attributes.route : '') + '.json')
 };
