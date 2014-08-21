3458fce297892d4fbe99644393c4a91f168fb507
Victor Nystad <vnystad@gmail.com>
2014-08-21 12:58:46 +0200

    tuning

--- src/create-url.js
+++ src/create-url.js
@@ -1,7 +1,5 @@
 function createUrl(attributes) {
-  return new Promise(function (resolve, reject) {
-    resolve('http://api.fotballdata.no/v1/' + attributes.operation + '/' + attributes.key + (!!attributes.route ? '/' + attributes.route : '') + '.json')
-  })        
+  return Promise.resolve('http://api.fotballdata.no/v1/' + attributes.operation + '/' + attributes.key + (!!attributes.route ? '/' + attributes.route : '') + '.json')
 };
 
 module.exports = createUrl;
