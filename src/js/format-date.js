306321ccf04835bb2881db52e53ab1090ce192ba
Victor Nystad <vnystad@gmail.com>
2014-08-24 10:40:58 +0200

    date formatting

--- src/js/format-date.js
+++ src/js/format-date.js
@@ -1,12 +1,8 @@
 var handlebars = require('hbsfy/runtime');
 
 function formatDate(date) {
-    // console.log(typeof date);
-    // var d =
-    // var d = date.match(/([0-9-])+/)[0];
-    // console.log(d typeof Number);
-    // console.log(new Date(Number());
-    return date;
+    var d = date.match(/([0-9])+/)[0];
+    return new Date(d);
 };
 
 module.exports = handlebars.registerHelper('formatDate', formatDate);
